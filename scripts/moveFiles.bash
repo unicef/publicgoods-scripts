c036d58099ec9b0225c0dd5e8a990c0e7be8f040
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2021-07-10 10:14:03 +0000

    updated moveFiles to include fonts folder and packages/eligibility build

--- scripts/moveFiles.bash
+++ scripts/moveFiles.bash
@@ -7,10 +7,13 @@ pushd ../publicgoods-website/registry && \
     cp ../../publicgoods-scripts/packages/registry/build/index.html . && \
 popd
 pushd ../publicgoods-website/eligibility && \
+    cp -R ../../publicgoods-website/wp-content/themes/dpga/font/. ../../publicgoods-scripts/packages/eligibility/src/font/ && \
+popd
+pushd packages/eligibility && npm install && npm run build && popd
+pushd ../publicgoods-website/eligibility && \
 	git rm -rf static/* || true && \
     mkdir static && \
     cp -a ../../publicgoods-scripts/packages/eligibility/build/static/* static && \
     git add static/* && \
     cp ../../publicgoods-scripts/packages/eligibility/build/index.html . && \
 popd
-
